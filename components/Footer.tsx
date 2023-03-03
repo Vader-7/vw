18418b04150c7ef1da297ba38fc983aed55c9ecf
タイラー <66812754+Vader-7@users.noreply.github.com>
2023-03-03 04:11:35 -0300

    fix: px Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,7 +11,7 @@ export function Footer() {
         </p>
       </div>
       <Separator className="my-2" />
-      <div className="flex h-4 items-center space-x-4 text-center text-sm font-medium subpixel-antialiased px-2">
+      <div className="flex h-4 items-center space-x-4 text-center text-sm font-medium subpixel-antialiased px-auto lg:px-2">
         <div>
           <a href="https://github.com/Vader-7" target="_blank">
             Github
