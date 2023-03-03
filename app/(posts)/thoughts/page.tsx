ba62e347510edf4df37db7d8d18232e47bcaf00c
タイラー <66812754+Vader-7@users.noreply.github.com>
2023-03-03 20:10:05 -0300

    fix: revalidate on post page

--- app/(posts)/thoughts/page.tsx
+++ app/(posts)/thoughts/page.tsx
@@ -3,14 +3,21 @@ import { BlogCard } from "@/components/articles/BlogCard"
 
 export const metadata = {
   title: "Thoughts",
-  description: "Some of the things I like to think about",
 }
 
 export default async function ThoughtsPage() {
-  const database = await getDatabase()
+  const [staticData, dynamicData] = await Promise.all([
+    getDatabase(),
+    {
+      next: {
+        revalidate: 10,
+      }
+    },
+  ])
+
   return (
     <div className="grid grid-cols-1 gap-7 lg:grid-cols-2 2xl:gap-12 ">
-      {database.map((page) => (
+      {staticData.map((page) => (
         <BlogCard post={page} key={page.id} />
       ))}
     </div>
