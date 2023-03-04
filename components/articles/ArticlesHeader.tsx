9e33c73b5c44d4efaaefe35fe34df844b1cee063
タイラー <66812754+Vader-7@users.noreply.github.com>
2023-03-04 06:12:22 -0300

    feat: added transition on post

--- components/articles/ArticlesHeader.tsx
+++ components/articles/ArticlesHeader.tsx
@@ -3,8 +3,24 @@
 import React, { FunctionComponent, useEffect, useState } from "react"
 import dayjs from "dayjs"
 import { IoSparklesSharp } from "react-icons/io5"
+import { motion } from "framer-motion"
 
 const localizedFormat = require("dayjs/plugin/localizedFormat")
+
+const staggerChildren = {
+  hidden: {
+    x: -10,
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: "easeInOut",
+    },
+  },
+}
 
 export const ArticlesHeader: FunctionComponent<{ page: any }> = ({ page }) => {
   const [author, setAuthor] = useState<any>(
@@ -22,12 +38,14 @@ export const ArticlesHeader: FunctionComponent<{ page: any }> = ({ page }) => {
   }, [page.properties.Author.people])
 
   return (
-    <h4 className="text-base font-medium text-slate-900 dark:text-slate-50 pb-[1rem]">
-      {dayjs(page.last_edited_time).format("dddd D / MMM / YYYY")}
-      <br />
-      <span className="text-md flex items-center font-semibold">
-        by&nbsp;{author}
-      </span>
-    </h4>
+    <motion.div variants={staggerChildren}>
+      <h4 className="text-base font-medium text-slate-900 dark:text-slate-50 pb-[1rem]">
+        {dayjs(page.last_edited_time).format("dddd D / MMM / YYYY")}
+        <br />
+        <span className="text-md flex items-center font-semibold">
+          by&nbsp;{author}
+        </span>
+      </h4>
+    </motion.div>
   )
 }
