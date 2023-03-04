10c27e4c3876bd8ae1996062d98ad3439f50b9fc
タイラー <66812754+Vader-7@users.noreply.github.com>
2023-03-04 04:11:17 -0300

    fix: index title

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: "Ty's Portfolio",
+}
+
+interface HomeLayoutProps {
+    children?: React.ReactNode
+}
+
+export default function HomeLayout({ children }: HomeLayoutProps) {
+    return <> {children} </>
+}
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -43,42 +43,39 @@ export default function Home() {
   const [isButtonClicked, setIsButtonClicked] = React.useState(false)
 
   return (
-    <>
-      <motion.div
-        className="flex h-screen flex-col items-center justify-center"
-        variants={variant}
-        initial="hidden"
-        animate="visible"
-      >
-        <div
-          className={`h-64 w-64 lg:h-96 lg:w-1/2 ${
-            isButtonClicked
-              ? "mt-10 scale-x-[1.7] scale-y-[2] transition-all duration-1000"
-              : "transition-all duration-1000"
+    <motion.div
+      className="flex h-screen flex-col items-center justify-center"
+      variants={variant}
+      initial="hidden"
+      animate="visible"
+    >
+      <div
+        className={`h-64 w-64 lg:h-96 lg:w-1/2 ${isButtonClicked
+          ? "mt-10 scale-x-[1.7] scale-y-[2] transition-all duration-1000"
+          : "transition-all duration-1000"
           }
             }`}
-          onClick={() => setIsButtonClicked(false)}
+        onClick={() => setIsButtonClicked(false)}
+      >
+        <Canvas />
+      </div>
+      <motion.div
+        className="align-center flex items-center gap-[12rem] pt-5 text-base font-semibold"
+        variants={item}
+      >
+        <Link
+          href="/about"
+          className="flex items-center justify-center gap-2 transition-colors duration-1000 hover:text-zinc-400"
         >
-          <Canvas />
-        </div>
-        <motion.div
-          className="align-center flex items-center gap-[5rem] pt-5 text-base font-semibold"
-          variants={item}
+          GET INSIDE
+        </Link>
+        <button
+          className="hidden lg:block"
+          onClick={() => setIsButtonClicked(true)}
         >
-          <Link
-            href="/about"
-            className="flex items-center justify-center gap-2 transition-colors duration-1000 hover:text-zinc-400"
-          >
-            GET INSIDE &rarr;
-          </Link>
-          <button
-            className="hidden lg:block"
-            onClick={() => setIsButtonClicked(true)}
-          >
-            <FontAwesomeIcon icon={faEye} />
-          </button>
-        </motion.div>
+          <FontAwesomeIcon icon={faEye} />
+        </button>
       </motion.div>
-    </>
+    </motion.div>
   )
 }
